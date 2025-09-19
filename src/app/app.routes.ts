acd7427030ef4c4c39bbdf44389347e8178a7f55
Denys Vladymyrov <link_amd@yahoo.com>
2025-09-19 21:53:52 +0300

    added title router resolver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import {resolveUserName, UserTasksComponent} from "./users/user-tasks/user-tasks.component";
+import {resolveTitle, resolveUserName, UserTasksComponent} from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: NoTaskComponent
+    component: NoTaskComponent,
+    title: 'No task selected'
   },
   {
     path: 'users/:userId',      // <your-domain>/users/<uid>
@@ -19,6 +20,7 @@ export const routes: Routes = [
     resolve: {
       userName: resolveUserName //
     },
+    title: resolveTitle,
     children: [
       {
         path: '',
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, DestroyRef, inject, input, OnInit, signal} from '@angular/core';
+import {Component, computed, DestroyRef, inject, input, OnDestroy, OnInit, signal} from '@angular/core';
 import {UsersService} from "../users.service";
 import {
   ActivatedRoute,
@@ -20,7 +20,7 @@ import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
     RouterLink
   ]
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
   userId = input.required<string>();
   message = input.required<string>();
   userName = input.required<string>();
@@ -29,6 +29,14 @@ export class UserTasksComponent {
   private activatedRoute = inject(ActivatedRoute);
   userNameObservable = signal('');
   private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.activatedRoute.data
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((data) => {
+      console.log(data);
+    })
+  }
 
   // userName = computed(() => this.usersService.users.find((user) => user.id === this.userId())?.name);
 
@@ -49,4 +57,11 @@ export const resolveUserName: ResolveFn<string> = (
 ) => {
   const usersService = inject(UsersService);
   return usersService.users.find((user) => user.id === activatedRoute.paramMap.get('userId'))?.name || '';
+}
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  return resolveUserName(activatedRoute, routerState) + '\'s Tasks'; // Max's Tasks
 }
