c610401a2fbcd6ed68d1fe65644044a523620591
Denys Vladymyrov <link_amd@yahoo.com>
2025-09-19 12:41:21 +0300

    first commit

---
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,31 @@
+import {Component, computed, DestroyRef, inject, input, OnInit, signal} from '@angular/core';
+import {UsersService} from "../users.service";
+import {ActivatedRoute, RouterOutlet} from "@angular/router";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+  imports: [
+    RouterOutlet
+  ]
+})
+export class UserTasksComponent implements OnInit {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  userNameObservable = signal('');
+  private destroyRef = inject(DestroyRef);
+
+  userName = computed(() => this.usersService.users.find((user) => user.id === this.userId())?.name);
+
+  ngOnInit() {
+    this.activatedRoute.paramMap
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(paramMap => {
+        this.userNameObservable.set(this.usersService.users.find((user) => user.id === paramMap.get('userId'))?.name || '')
+    })
+  }
+}
